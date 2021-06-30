9fac20ac494456c3b5538b0fa6c8d3b30a3eacae
octave collombel <octavec@outlook.com>
2021-06-30 22:39:14 +0200

    tests

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
-'''
-Defines the FileStorage class
-'''
+"""
+Unittest to test FileStorage class
+"""
+import unittest
 import json
+import os
+import shutil
 from models.base_model import BaseModel
 from models.user import User
 from models.state import State
@@ -10,70 +13,82 @@ from models.city import City
 from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
+from models.engine.file_storage import FileStorage
 
 
-class FileStorage:
-    '''Class that stores objects in JSON strings
-    Attributes:
-    file_path (str): path to the JSON file
-    objects (dict): stores all objects by class
-    '''
-
-    __file_path = 'file.json'
-    __objects = {}
-    class_dict = {
-        "BaseModel": BaseModel,
-        "User": User,
-        "State": State,
-        "City": City,
-        "Amenity": Amenity,
-        "Place": Place,
-        "Review": Review}
-
-    def all(self):
-        '''Returns the dictionary __objects
-        Return:
-            returns __objects
-        '''
-        return FileStorage.__objects
-
-    def new(self, obj):
-        '''Sets new object in __objects dictionary
-        '''
-        k = '{}.{}'.format(obj.__class__.__name__, obj.id)
-        FileStorage.__objects[k] = obj
-
-    def save(self):
-        '''Serializes __objects to the JSON file
-        '''
-        save_dict = {}
-        for k, v in FileStorage.__objects.items():
-            v_dict = v.to_dict()
-            save_dict[k] = v_dict
-        with open(FileStorage.__file_path, 'w') as f:
-            json.dump(save_dict, f)
-
-    def reload(self):
-        '''Deserialize the JSON file to __objects
-        '''
-        try:
-            with open(FileStorage.__file_path, 'r') as f:
-                dicts = json.load(f)
-                FileStorage.__objects = {}
-                for k, v in dicts.items():
-                    obj = FileStorage.class_dict[v['__class__']](**v)
-                    FileStorage.__objects[k] = obj
-        except FileNotFoundError:
-            return
+class TestFileStorage(unittest.TestCase):
+    '''testing file storage'''
 
     @classmethod
-    def get_object(cls, id=''):
-        '''Returns an object based on id
-        Return:
-        returns an object that matches id or prints an error on failure
-        '''
-        objects = cls.__objects
-        for obj in objects.values():
-            if obj.id == id:
-                return obj
-        print("id could not be matched")
+    def setUpClass(cls):
+        cls.rev1 = Review()
+        cls.rev1.place_id = "Raleigh"
+        cls.rev1.user_id = "Greg"
+        cls.rev1.text = "Grade A"
+
+    @classmethod
+    def teardown(cls):
+        del cls.rev1
+
+    def teardown(self):
+        try:
+            os.remove("file.json")
+        except Exception:
+            pass
+
+    def test_all(self):
+        """
+        Tests method: all (returns dictionary <class>.<id> : <obj instance>)
+        """
+        storage = FileStorage()
+        instances_dic = storage.all()
+        self.assertIsNotNone(instances_dic)
+        self.assertEqual(type(instances_dic), dict)
+        self.assertIs(instances_dic, storage._FileStorage__objects)
+
+    def test_new(self):
+        """
+        Tests method: new (saves new object into dictionary)
+        """
+        m_storage = FileStorage()
+        instances_dic = m_storage.all()
+        melissa = User()
+        melissa.id = 999999
+        melissa.name = "Melissa"
+        m_storage.new(melissa)
+        key = melissa.__class__.__name__ + "." + str(melissa.id)
+        self.assertIsNotNone(instances_dic[key])
+
+    def test_reload_empty(self):
+        """
+        Tests method: reload (reloads objects from string file)
+        """
+        a_storage = FileStorage()
+        try:
+            os.remove("file.json")
+        except Exception:
+            pass
+        with open("file.json", "w") as f:
+            f.write("{}")
+        with open("file.json", "r") as r:
+            for line in r:
+                self.assertEqual(line, "{}")
+        self.assertIs(a_storage.reload(), None)
+
+    def test_reload(self):
+        """
+        Tests method: reload (reloads objects from string file)
+        """
+        self.maxDiff = None
+        a_storage = FileStorage()
+        try:
+            os.remove("file.json")
+        except Exception:
+            pass
+        shutil.copy("./tests/test_models/test_engine/allin.txt", "./file.json")
+        with open("./file.json") as f:
+            dicts = json.load(f)
+        a_storage.reload()
+        objs = a_storage.all()
+        for key in dicts:
+            self.assertEqual(objs[key].to_dict(), dicts[key])
