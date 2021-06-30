7e81c698d7dfb11bac5568ce5a9bbea74ce46047
octave collombel <octavec@outlook.com>
2021-06-30 06:07:06 +0200

    tests

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -3,23 +3,17 @@
 Unittests for the project
 """
 
-import os
 import unittest
-import json
+import os.path
+from models import storage
 from models.base_model import BaseModel
-from models.engine.file_storage import FileStorage
+from models.user import User
+from models.state import State
+from models.city import City
 from models.amenity import Amenity
 from models.place import Place
-from models.state import State
 from models.review import Review
-from models.user import User
-from models.city import City
-from models import storage
-import inspect
-import time
-from datetime import datetime
-import models
-from os import path
+from models.engine.file_storage import FileStorage
 
 
 class TestsForFileStorage(unittest.TestCase):
