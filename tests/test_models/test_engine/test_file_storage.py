706857ff71e8e600a087d9e453057dd43094dabf
octave collombel <octavec@outlook.com>
2021-06-30 05:44:12 +0200

    tests

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -25,115 +25,54 @@ from os import path
 class TestsForFileStorage(unittest.TestCase):
     """ Tests for the Base Class """
 
-    def test_reload(self):
-        """ Testing reload() """
+    def test_doc(self):
+        """ Testing for docstring """
+        self.assertIsNotNone(("models.engine.file_storage".__doc__))
+        self.assertIsNotNone(FileStorage.__doc__)
+        self.assertIsNotNone(FileStorage.__init__.__doc__)
+        self.assertIsNotNone(FileStorage.all.__doc__)
+        self.assertIsNotNone(FileStorage.new.__doc__)
+        self.assertIsNotNone(FileStorage.save.__doc__)
+        self.assertIsNotNone(FileStorage.reload.__doc__)
+
+    def test_atr(self):
+        """ Tests for attributes """
+        s1 = FileStorage()
+        self.assertTrue(hasattr(s1, "_FileStorage__objects"))
+        self.assertTrue(hasattr(s1, "_FileStorage__file_path"))
+
+    def test_class(self):
+        """ Makes sure we're making the class """
+        s1 = FileStorage()
+        self.assertTrue(type(s1), FileStorage)
+
+    def test_all_new(self):
+        """ Tests the functionality of new and all """
+        setattr(storage, "_FileStorage__objects", dict())
+        m1 = BaseModel()
+        m2 = BaseModel()
+        new_dict = dict()
+        new_dict["BaseModel." + m1.id] = m1
+        new_dict["BaseModel." + m2.id] = m2
+        thing = storage.all()
+        self.assertDictEqual(thing, new_dict)
+
+    def test_save_reload(self):
+        """ Test save and reload """
         os.remove('file.json')
-        new_base = BaseModel()
-        new_base.save()
+        b1 = BaseModel()
+        b1.save()
         storage.reload()
         new = storage.all()
-        self.assertDictEqual(new["BaseModel." + new_base.id].to_dict(),
-                             new_base.to_dict())
+        self.assertDictEqual(new["BaseModel." + b1.id].to_dict(), b1.to_dict())
 
-    def test_file_path(self):
-        """ Testing file path """
-        self.assertEqual(str, type(FileStorage._FileStorage__file_path))
-
-    def test_all(self):
-        """ Testing all() """
-        new_file = FileStorage()
-        new_base = BaseModel(id="564778",
-                             created_at="2021-07-02T22:46:38.849900",
-                             updated_at="2021-07-02T22:46:38.849900")
-        self.assertIsInstance(new_file, FileStorage)
-        self.assertIs(type(new_file), FileStorage)
-        objects = new_file.all()
-        self.assertIs(type(objects), dict)
-
-    def test_new(self):
-        """ Testing new() """
-        new_file = FileStorage()
-        new_base = BaseModel(id="564778",
-                             created_at="2021-07-02T22:46:38.849900",
-                             updated_at="2021-07-02T22:46:38.849900")
-        new_city = City()
-        new_amenity = Amenity()
-        new_user = User()
-        new_place = Place()
-        new_review = Review()
-        new_state = State()
-
-        new_file.new(new_base)
-        new_file.new(new_city)
-        new_file.new(new_amenity)
-        new_file.new(new_place)
-        new_file.new(new_state)
-        new_file.new(new_user)
-        new_file.new(new_review)
-
-        objects = new_file.all()
-
-        key = new_base.__class__.__name__ + "." + new_base.__dict__["id"]
-        key_city = new_city.__class__.__name__ + "." + new_city.__dict__["id"]
-        key_user = new_user.__class__.__name__ + "." + new_user.__dict__["id"]
-        key_review = new_review.__class__.__name__ + "." +\
-            new_review.__dict__["id"]
-
-        key_place = new_place.__class__.__name__ + "."\
-            + new_place.__dict__["id"]
-
-        key_state = new_state.__class__.__name__ + "."\
-            + new_state.__dict__["id"]
-
-        key_amenity = new_amenity.__class__.__name__ + "."\
-            + new_amenity.__dict__["id"]
-
-        self.assertIn(key, objects)
-        self.assertIn(key_city, objects)
-        self.assertIn(key_user, objects)
-        self.assertIn(key_review, objects)
-        self.assertIn(key_place, objects)
-        self.assertIn(key_state, objects)
-        self.assertIn(key_amenity, objects)
-
-    def test_save(self):
-        """ Testing save() """
-        objects = models.storage
-
-        new_base = BaseModel()
-        new_user = User()
-        new_state = State()
-        new_place = Place()
-        new_city = City()
-        new_amenity = Amenity()
-        new_review = Review()
-
-        objects.new(new_base)
-        objects.new(new_user)
-        objects.new(new_state)
-        objects.new(new_place)
-        objects.new(new_city)
-        objects.new(new_amenity)
-        objects.new(new_review)
-        objects.save()
-
-        save_text = ""
-        with open("file.json", "r") as f:
-            save_text = f.read()
-            self.assertIn("BaseModel." + new_base.id, save_text)
-            self.assertIn("User." + new_user.id, save_text)
-            self.assertIn("State." + new_state.id, save_text)
-            self.assertIn("Place." + new_place.id, save_text)
-            self.assertIn("City." + new_city.id, save_text)
-            self.assertIn("Amenity." + new_amenity.id, save_text)
-            self.assertIn("Review." + new_review.id, save_text)
-
-    def test_reload_2(self):
-        """ Testing reload() """
-        os.remove('file.json')
-        new_base = BaseModel()
-        new_base.save()
-        storage.reload()
-        new = storage.all()
-        self.assertDictEqual(new["BaseModel." + new_base.id].to_dict(),
-                             new_base.to_dict())
+    def test_save_andrew_kali_suggestion(self):
+        """
+            test save using method provided by peer in Bog
+            not our own test
+        """
+        o = BaseModel()
+        time.sleep(1)
+        n = datetime.datetime.now().replace(microsecond=0)
+        o.save()
+        self.assertEqual(o.updated_at.replace(microsecond=0), n)
